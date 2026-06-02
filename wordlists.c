1868ecb1c2fe490575aa3097316404a4eb996f11
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-06-02 09:34:41 +0300

    Update wordlists.c

--- wordlists.c
+++ wordlists.c
@@ -22,18 +22,15 @@ struct wordlists *read_wordlists(const char *filename){
     struct wordlists *words = NULL;
     char buf[LEN] = {'\0'};
     FILE *file = NULL;
-    size_t len = 0;
     if((file = fopen(filename, "r")) != NULL){
         if((words = (struct wordlists*)malloc(sizeof(struct wordlists))) != NULL){
             words->count = 0;
             words->words = NULL;
             while(fgets(buf, sizeof(buf), file) != NULL){
-                len = strlen(buf);
-                if(len > 0){
-                    if(strcspn(buf, "\n") != len)
-                        buf[strlen(buf) - 1] = '\0';
-                    add_words(words, buf);
-                }                
+                buf[strcspn(buf, "\r\n")] = '\0';
+                if(buf[0] == '\0')
+                    continue;
+                add_words(words, buf);               
             }
             fclose(file);
             return words;
